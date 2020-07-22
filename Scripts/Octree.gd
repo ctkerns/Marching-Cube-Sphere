75ac8156722eeda26543265a071312006f5753d7
Christopher Kerns <chmos13@yahoo.com>
2020-07-21 22:00:20 -0400

    Added function for drawing octree

--- Scripts/Octree.gd
+++ Scripts/Octree.gd
@@ -1,16 +1,15 @@
 extends Spatial
 
 var Octnode = preload("res://Scripts/Octree/Octnode.gd")
-var Octleaf = preload("res://Scripts/Octree/Octleaf.gd")
 
 onready var mesh_instance = get_node("MeshInstance")
 
-var head
-var _tree_depth
+onready var head = Octnode.Octnode.new()
 
-func init(tree_depth):
-	pass
-	# Do nothing.
+func init():
+	head._split()
+	head._get_child(0)._split()
+	head._get_child(0)._get_child(0)._split()
 	
 func draw():
 	# Set up array mesh.
@@ -19,10 +18,57 @@ func draw():
 	var verts = PoolVector3Array()
 
 	# Draw octree.
-	verts = Geometry.draw_bounds(Vector3(-1, -1, 1), Vector3(1, 1, 2), verts)
+	verts = _draw_tree(verts)
 
 	# Create surface.
 	arr[Mesh.ARRAY_VERTEX] = verts
 
 	mesh_instance.mesh = ArrayMesh.new()
 	mesh_instance.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_LINES, arr)
+
+func _draw_tree(verts):
+	var stack = []
+	stack.push_back(head)
+	
+	var vert = Vector3(-1, -1, 1)
+	var vert_stack = [vert]
+
+	var edge = 2.0
+	var edge_stack = [edge]
+	
+	var arb_factor = 4
+
+	# Perform a DFS traversal of the octree.
+	var n
+	while (!stack.empty()):
+		# Pop frame from stack.
+		n = stack.pop_back()
+		vert = vert_stack.pop_back()
+		edge = edge_stack.pop_back()
+
+		if (n._is_leaf()):
+			# Draw the leaf node.
+			var arb_stretch = Vector3(vert.x, vert.y, vert.z*arb_factor)
+			
+			verts = Geometry.draw_bounds(arb_stretch, arb_stretch + Vector3(edge, edge, edge*arb_factor), verts)
+		else:
+			# Add this nodes children to the stack.
+			edge /= 2
+
+			for i in range(8):
+				# Calculate the first vertex of each child node.
+				var new_vert = vert
+				
+				if i & 4 == 4:
+					new_vert.x += edge
+				if i & 2 == 2:
+					new_vert.y += edge
+				if i & 1 == 1:
+					new_vert.z += edge
+				
+				# Push frame onto stack.
+				stack.push_back(n._get_child(i))
+				vert_stack.push_back(new_vert)
+				edge_stack.push_back(edge)
+
+	return verts
--- Scripts/Octree/Octnode.gd
+++ Scripts/Octree/Octnode.gd
@@ -1,7 +1,18 @@
 extends Object
 
-var Octleaf = preload("res://Scripts/Octree/Octleaf.gd")
-
 class Octnode:
+	var _children = []
+
 	func _init():
 		pass
+	
+	func _split():
+		for _i in range(8):
+			_children.append(get_script().new())
+			
+	func _get_child(idx):
+		if (_children.size() > idx):
+			return _children[idx]
+
+	func _is_leaf():
+		return _children.size() == 0
