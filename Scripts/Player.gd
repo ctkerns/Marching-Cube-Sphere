9bee0c9cb8762daa98227dff9370e228f0f89fc7
Christopher Kerns <chmos13@yahoo.com>
2020-07-24 03:50:24 -0400

    Player moves spherically The player will now move spherically with reference to the parent node

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,8 +3,8 @@ extends Spatial
 var _speed = 5
 var _mouse_sensitivity = 0.1
 
-onready var rotator = $Rotator
-onready var camera = $Rotator/Camera
+onready var pitch = $PitchRotator
+onready var camera = $PitchRotator/Camera
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -31,13 +31,20 @@ func _process(delta):
 		translate(Vector3(0, _speed*delta, 0))
 	if Input.is_action_pressed("move_down"):
 		translate(Vector3(0, -_speed*delta, 0))
+		
+	# Update player rotation to spherical tangent.
+	var prev_forward = -self.get_transform().basis.z
+	var look_up = self.get_translation().normalized()
+	var look_forward = look_up.cross(prev_forward.cross(look_up)).normalized()
+	self.look_at(self.get_translation() + look_forward, look_up)
 
 func _input(event):
 	# Rotate the camera.
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		rotator.rotate_object_local(Vector3(1, 0, 0), deg2rad(event.relative.y*_mouse_sensitivity*-1))
+		pitch.rotate_object_local(Vector3(1, 0, 0), deg2rad(event.relative.y*_mouse_sensitivity*-1))
 		self.rotate_object_local(Vector3(0, 1, 0), deg2rad(event.relative.x*_mouse_sensitivity*-1))
 
-		var clamped_rotation = rotator.rotation_degrees
+		var clamped_rotation = pitch.rotation_degrees
 		clamped_rotation.x = clamp(clamped_rotation.x, -90, 90)
-		rotator.rotation_degrees = clamped_rotation
+		pitch.rotation_degrees = clamped_rotation
+		
