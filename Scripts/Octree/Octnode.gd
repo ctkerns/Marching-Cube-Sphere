fd2cff0202adb8e3d73bd9413c7aaf2040ad5528
Christopher Kerns <chmos13@yahoo.com>
2020-07-27 05:47:25 -0400

    Switched to linear octree

--- Scripts/Octree/Octnode.gd
+++ Scripts/Octree/Octnode.gd
@@ -1,18 +1,7 @@
 extends Object
 
 class Octnode:
-	var _children = []
+	var volume
 
-	func _init():
-		pass
-	
-	func _split():
-		for _i in range(8):
-			_children.append(get_script().new())
-			
-	func _get_child(idx):
-		if (_children.size() > idx):
-			return _children[idx]
-
-	func _is_leaf():
-		return _children.size() == 0
+	func _init(vol):
+		volume = vol
--- Scripts/Octree/Octree.gd
+++ Scripts/Octree/Octree.gd
@@ -1,15 +1,39 @@
-extends Spatial
+extends Planetary
 
 var Octnode = preload("res://Scripts/Octree/Octnode.gd")
 
 onready var mesh_instance = get_node("MeshInstance")
 
-onready var head = Octnode.Octnode.new()
+onready var head = Octnode.Octnode.new(0.5)
+
+enum Division {SW_down, NW_down, SW_up, NW_up, SE_down, NE_down, SE_up, NE_up}
+
+var nodes: Dictionary
 
 func init():
-	head._split()
-	head._get_child(0)._split()
-	head._get_child(0)._get_child(0)._split()
+	nodes[1] = Octnode.Octnode.new(0.5)
+	
+	#split(0b1)
+	#split(0b1000)
+	#split(0b1000000)
+	pass
+	
+func split(loc_code: int):
+	if _depth(loc_code) > 20:
+		return
+		
+	var prefix = loc_code << 3
+	var suffix = 0
+	
+	for i in range(8):
+		nodes[prefix | suffix] = Octnode.Octnode.new(0.5)
+		suffix += 1
+	
+func _depth(loc_code: int) -> int:
+	return int( (log(loc_code) / 3) / log(2) )
+	
+func _get_child(loc_code: int, dir: int) -> int:
+	return loc_code << 3 | dir
 	
 func draw():
 	# Set up array mesh.
@@ -27,8 +51,9 @@ func draw():
 	mesh_instance.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_LINES, arr)
 
 func _draw_tree(verts):
+	# Initialize stack.
 	var stack = []
-	stack.push_back(head)
+	stack.push_back(1)
 	
 	var vert = Vector3(-1, -1, 1)
 	var vert_stack = [vert]
@@ -38,15 +63,19 @@ func _draw_tree(verts):
 	
 	var arb_factor = 4
 
-	# Perform a DFS traversal of the octree.
-	var n
-	while (!stack.empty()):
+	# Perform a DFS traversal of the octree using stack.
+	var id; var n
+	while !stack.empty():
 		# Pop frame from stack.
-		n = stack.pop_back()
+		id = stack.pop_back()
 		vert = vert_stack.pop_back()
 		edge = edge_stack.pop_back()
 
-		if (n._is_leaf()):
+		# Get the address of a child of this node.
+		var check = _get_child(id, 0)
+
+		# Check if this node has children.
+		if !nodes.has(check):
 			# Draw the leaf node.
 			var arb_stretch = Vector3(vert.x, vert.y, vert.z*arb_factor)
 			
@@ -59,6 +88,8 @@ func _draw_tree(verts):
 				# Calculate the first vertex of each child node.
 				var new_vert = vert
 				
+				# No idea if this is accurate to the enum direction I set up.
+				# Double check this.
 				if i & 4 == 4:
 					new_vert.x += edge
 				if i & 2 == 2:
@@ -67,7 +98,7 @@ func _draw_tree(verts):
 					new_vert.z += edge
 				
 				# Push frame onto stack.
-				stack.push_back(n._get_child(i))
+				stack.push_back(_get_child(id, i))
 				vert_stack.push_back(new_vert)
 				edge_stack.push_back(edge)
 
