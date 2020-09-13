820c5bf8871851cc988aec8b5d61717c58faa571
Christopher Kerns <chmos13@yahoo.com>
2020-09-12 22:35:05 -0400

    Added gravity and player collision.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,14 +1,18 @@
-extends Spatial
+extends KinematicBody
 
 var _speed = 5
+var _acceleataion = 3
 var _mouse_sensitivity = 0.1
+var _velocity = Vector3()
+var _grounded = false
+var _friction = 0.99
 
 onready var pitch = $PitchRotator
 onready var camera = $PitchRotator/Camera
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	self.set_rotation(Vector3(0.0, 0.0, 0.0))
+	self.set_rotation(Vector3())
 	
 func _process(delta):
 	# Free the mouse.
@@ -17,26 +21,47 @@ func _process(delta):
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	
-	# Move the Player.
-	if Input.is_action_pressed("move_forward"):
-		translate(Vector3(0, 0, -_speed*delta))
-	if Input.is_action_pressed("move_backward"):
-		translate(Vector3(0, 0, _speed*delta))
-	if Input.is_action_pressed("move_left"):
-		translate(Vector3(-_speed*delta, 0, 0))
-	if Input.is_action_pressed("move_right"):
-		translate(Vector3(_speed*delta, 0, 0))
-	if Input.is_action_pressed("move_up"):
-		translate(Vector3(0, _speed*delta, 0))
-	if Input.is_action_pressed("move_down"):
-		translate(Vector3(0, -_speed*delta, 0))
 		
 	# Update player rotation to spherical tangent.
 	var prev_forward = -self.get_transform().basis.z
 	var look_up = self.get_translation().normalized()
 	var look_forward = look_up.cross(prev_forward.cross(look_up)).normalized()
 	self.look_at(self.get_translation() + look_forward, look_up)
+	
+func _physics_process(delta):
+	var up
+	var direction = Vector3()
+	var forward = self.get_global_transform().basis
+	
+	# Find the player's intended direction.
+	if _grounded:
+		if Input.is_action_pressed("move_forward"):
+			direction -= forward.z
+		if Input.is_action_pressed("move_backward"):
+			direction += forward.z
+		if Input.is_action_pressed("move_left"):
+			direction -= forward.x
+		if Input.is_action_pressed("move_right"):
+			direction += forward.x
+		if Input.is_action_pressed("move_up"):
+			up = self.get_translation().normalized()
+			_velocity += up*10.0
+	
+		# Walk.
+		direction = direction.normalized()*_speed
+		_velocity = _velocity.linear_interpolate(direction, _speed*delta)
+	
+	# Do the gravity.
+	up = self.get_translation().normalized()
+	_velocity -= up*9.8*delta
+	
+	# Collide.
+	var collision = move_and_collide(_velocity*delta)
+	up = self.get_translation().normalized()
+	_grounded = false
+	if collision != null:
+		_velocity -= _velocity.project(-up)
+		_grounded = true
 
 func _input(event):
 	# Rotate the camera.
