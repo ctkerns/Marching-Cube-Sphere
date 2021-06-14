ae838b9ac3b7584de94d5b1c0ea74c61af9158bc
Christopher Kerns <chmos13@yahoo.com>
2021-06-14 02:20:54 -0400

    Added granularity

--- Scripts/Singletons/Generator.gd
+++ Scripts/Singletons/Generator.gd
@@ -14,7 +14,7 @@ func sample(x: float, y: float, z: float, base, top) -> float:
 
 	var magnitude = Vector3(x, y, z).length()
 
-	vol = (magnitude - top)/(base - top) + vol/1.0
+	vol = (magnitude - top)/(base - top) + vol/1.0 + (randf()*2.0 - 1.0)/48.0 
 
 	if vol > 1.0:
 		vol = 1.0
