59785fa44596b1eb84c1775eece96c25a995985c
Christopher Kerns <chmos13@yahoo.com>
2021-06-16 14:53:25 -0400

    Remove full_subdivision

--- Scripts/Octree/OctTerrain.gd
+++ Scripts/Octree/OctTerrain.gd
@@ -78,30 +78,3 @@ func _generate():
 			if homogenous and all_leaves:
 				for k in range(8):
 					octree.delete_node(octree.get_child(node, k))
-
-# Fully subdivide the tree.
-func _full_subdivision():
-	var queue = []
-	queue.push_back(0b1)
-
-	# Subdivide each node for each level in depth.
-	for _i in range(_depth):
-		for _j in range(queue.size()):
-			# Find the volume for each child before they are created.
-			var node = queue.pop_front()
-			var volumes = []
-			for k in range(8):
-				var child = octree.get_child(node, k)
-				var vert = octree.get_vertex(child)
-
-				# This needs to change if the planet is going to move.
-				vert = self.to_global(vert)
-
-				volumes.append(Generator.sample(vert.x, vert.y, vert.z, 1, 3))
-				
-			# Split each node in the queue, and add the nodes to the queue.
-			octree.split(node, volumes)
-
-			var prefix = node << 3
-			for k in range(8):
-				queue.push_back(prefix | k)	
