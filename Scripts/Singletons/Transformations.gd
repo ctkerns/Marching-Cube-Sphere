c8192e39924ef4e9792f52a2cc6e6476625ad0e0
Christopher Kerns <chmos13@yahoo.com>
2021-06-17 15:39:52 -0400

    Remove Transformation class. no longer needed.

--- Scripts/Singletons/Transformations.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-# Takes a location in the local cube and converts it to global space.
-func cube2global(vert: Vector3, transform: Transform):
-	var local_vert = vert #cube2sphere(vert.x, vert.y, vert.z)
-	var global_vert = transform.xform(local_vert)
-
-	return global_vert
-	
